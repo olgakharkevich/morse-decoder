7d1a5fed553a90f92532d5b83fea5394d8ac8236
olgakharkevich <Olga.Kharkevich@gmail.com>
2020-03-22 16:20:59 +0300

    fix: Morse code decoder

--- src/index.js
+++ src/index.js
@@ -37,8 +37,33 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+function _chunk(str, n) {
+    let ret = [];
+    let i;
+    let len;
+    for(i = 0, len = str.length; i < len; i += n) {
+       ret.push(str.substr(i, n))
+    }
+    return ret
+};
+
 function decode(expr) {
-    // write your solution here
+    let arr = _chunk(expr, 10);
+    let _res = String();
+    arr.forEach(el => {``
+        if (el == '**********') {_res += ' ';
+        } else {
+            el = Number(el).toString();
+            let str = _chunk(el, 2).join(',');
+            str = str.replace( /10/g, ".");
+            str = str.replace( /11/g, "-");
+            str = str.replace( /\,/g, '');
+            el = MORSE_TABLE[str];
+            _res += el;
+        }
+        
+    });
+    return _res;
 }
 
 module.exports = {
